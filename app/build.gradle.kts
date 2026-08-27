d34f4600ff934179fade4c32d6efc6c39cca5651
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-27 19:11:04 +0300

    Bump release to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.audoiboo.tracker"
         minSdk = 23
         targetSdk = 37
-        versionCode = 101
-        versionName = "1.0.1"
+        versionCode = 102
+        versionName = "1.0.2"
     }
 
     signingConfigs {

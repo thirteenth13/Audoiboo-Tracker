82dd66525bdd4890535c6817d0716bf193c125ce
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-29 21:49:34 +0300

    Test strict managed download payload validation

--- app/src/test/java/org/audoiboo/tracker/ManagedDownloadMigrationPolicyTest.kt
+++ app/src/test/java/org/audoiboo/tracker/ManagedDownloadMigrationPolicyTest.kt
@@ -16,4 +16,14 @@ class ManagedDownloadMigrationPolicyTest {
     @Test fun removesLegacyWhenRoomAlreadyOwnsState() {
         assertTrue(ManagedDownloadMigrationPolicy.shouldRemoveLegacy(roomCount = 1, legacyParsed = false))
     }
+
+    @Test fun acceptsEmptyAndFullyValidPayloads() {
+        assertTrue(ManagedDownloadMigrationPolicy.payloadIsComplete(sourceCount = 0, validCount = 0))
+        assertTrue(ManagedDownloadMigrationPolicy.payloadIsComplete(sourceCount = 3, validCount = 3))
+    }
+
+    @Test fun rejectsPartiallyParsedPayloads() {
+        assertFalse(ManagedDownloadMigrationPolicy.payloadIsComplete(sourceCount = 3, validCount = 2))
+        assertFalse(ManagedDownloadMigrationPolicy.payloadIsComplete(sourceCount = 1, validCount = 0))
+    }
 }

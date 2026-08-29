aae9c4e1f3446232cf00bea7cc076a3b4ee37c5e
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-30 00:01:26 +0300

    Test repeatable SAF migration decisions

---
+++ app/src/test/java/org/audoiboo/tracker/StorageMigrationPolicyTest.kt
@@ -0,0 +1,27 @@
+package org.audoiboo.tracker
+
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class StorageMigrationPolicyTest {
+    @Test fun sameUriIsAlwaysAlreadyMigrated() {
+        assertTrue(StorageMigrationPolicy.canReuseExisting("content://tree/file", "content://tree/file", null, 0))
+    }
+
+    @Test fun equalKnownNonzeroSizesReuseExistingDestination() {
+        assertTrue(StorageMigrationPolicy.canReuseExisting("old", "new", 1234, 1234))
+        assertFalse(StorageMigrationPolicy.canReuseExisting("old", "new", null, 1234))
+        assertFalse(StorageMigrationPolicy.canReuseExisting("old", "new", 0, 0))
+        assertFalse(StorageMigrationPolicy.canReuseExisting("old", "new", 1234, 1200))
+    }
+
+    @Test fun destinationRemovesOnlyLeadingDownloadSegment() {
+        assertEquals("Audoiboo/Series", StorageMigrationPolicy.normalizedDestination("Download/Audoiboo/Series"))
+        assertEquals("Audoiboo/Series", StorageMigrationPolicy.normalizedDestination("download\\Audoiboo\\Series"))
+        assertEquals("MyDownload/Book", StorageMigrationPolicy.normalizedDestination("MyDownload/Book"))
+        assertNull(StorageMigrationPolicy.normalizedDestination("Download/../Other"))
+    }
+}

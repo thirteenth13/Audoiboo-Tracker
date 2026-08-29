67ad975bc063525c20f14ce4202e9a8d0064b404
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-29 17:47:13 +0300

    Fix downloads screen Compose imports

--- app/src/main/java/org/audoiboo/tracker/ManagedDownloadsScreen.kt
+++ app/src/main/java/org/audoiboo/tracker/ManagedDownloadsScreen.kt
@@ -2,18 +2,7 @@ package org.audoiboo.tracker
 
 import android.content.Context
 import android.content.Intent
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.weight
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape

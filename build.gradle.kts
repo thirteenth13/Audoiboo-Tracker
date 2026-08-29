f25d653b824ebc2ae4b07fee5f26280c45467369
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-29 12:16:19 +0300

    Use plugin version catalog

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.android.application") version "9.3.0" apply false
-    id("org.jetbrains.kotlin.plugin.compose") version "2.3.21" apply false
-    id("com.google.devtools.ksp") version "2.3.9" apply false
+    alias(libs.plugins.android.application) apply false
+    alias(libs.plugins.kotlin.compose) apply false
+    alias(libs.plugins.ksp) apply false
 }

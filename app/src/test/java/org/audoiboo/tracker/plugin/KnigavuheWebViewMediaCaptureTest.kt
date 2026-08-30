f3dfe00843d7de7f327d8bafab7b7b4d442c36e7
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-30 21:51:39 +0300

    Test Knigavuhe WebView media URL filtering

---
+++ app/src/test/java/org/audoiboo/tracker/plugin/KnigavuheWebViewMediaCaptureTest.kt
@@ -0,0 +1,43 @@
+package org.audoiboo.tracker.plugin
+
+import org.junit.Assert.assertFalse
+import org.junit.Assert.assertTrue
+import org.junit.Test
+
+class KnigavuheWebViewMediaCaptureTest {
+    @Test fun acceptsDesktopBookAudio() {
+        assertTrue(KnigavuheWebViewMediaCapture.isBookAudio(
+            "https://u11.knigavuhe.org/1/audio/18350/hash/00.mp3"
+        ))
+    }
+
+    @Test fun acceptsMobileLargeSegment() {
+        assertTrue(KnigavuheWebViewMediaCapture.isBookAudio(
+            "https://u11.knigavuhe.org/1/audio/18350/mobile3/hash/igra-kota-kniga-vtoraja-4.mp3"
+        ))
+    }
+
+    @Test fun rejectsAdvertisingMedia() {
+        assertFalse(KnigavuheWebViewMediaCapture.isBookAudio(
+            "https://rutube.ru/video/ad-player.webm"
+        ))
+    }
+
+    @Test fun rejectsThirdPartyMp3() {
+        assertFalse(KnigavuheWebViewMediaCapture.isBookAudio(
+            "https://example.org/audio/18350/00.mp3"
+        ))
+    }
+
+    @Test fun rejectsNonAudioKnigavuheUrl() {
+        assertFalse(KnigavuheWebViewMediaCapture.isBookAudio(
+            "https://knigavuhe.org/book/igra-kota-kniga-vtoraja/"
+        ))
+    }
+
+    @Test fun acceptsMobileBookPage() {
+        assertTrue(KnigavuheWebViewMediaCapture.isAllowedPage(
+            "https://m.knigavuhe.org/book/igra-kota-kniga-vtoraja/"
+        ))
+    }
+}

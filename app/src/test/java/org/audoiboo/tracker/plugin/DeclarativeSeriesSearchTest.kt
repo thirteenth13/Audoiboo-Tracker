5efb119950d68f997c8defdf130c05e3e9fa9a83
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-08-31 18:27:04 +0300

    Test Knigavuhe series search selectors

--- app/src/test/java/org/audoiboo/tracker/plugin/DeclarativeSeriesSearchTest.kt
+++ app/src/test/java/org/audoiboo/tracker/plugin/DeclarativeSeriesSearchTest.kt
@@ -55,6 +55,53 @@ class DeclarativeSeriesSearchTest {
         assertTrue(results.all { it.series.sourceId == "search-test" })
     }
 
+    @Test
+    fun parsesCurrentKnigavuheSeriesLinksFromSearchResults() = withTempDir { root ->
+        File(root, "search.rule").writeText("search")
+        val sandbox = PluginSandbox(PluginHttpTransport { request, _ ->
+            PluginHttpResponse(
+                200,
+                request.url,
+                """
+                    <section>
+                      <a href='/series/zvezdnaja-krov/'>Звездная кровь</a>
+                      <a href='/book/zvezdnaja-krov-10/'>Звездная кровь 10</a>
+                      <a href='/serie/legacy-cycle/'>Legacy Cycle</a>
+                    </section>
+                """.trimIndent()
+            )
+        })
+        val runtime = DeclarativePluginRuntime(sandbox, DeclarativeEntrypointDecoder {
+            DeclarativeEntrypoint.SeriesSearch(
+                searchUrl = "https://knigavuhe.org/search/?q={query}",
+                items = RepeatedFields(
+                    item = "a[href*='/series/'], a[href*='/serie/']",
+                    title = "@text",
+                    link = "@href"
+                ),
+                maxResults = 12
+            )
+        })
+        val manifest = PluginPackageManifest(
+            id = "knigavuhe",
+            name = "Knigavuhe",
+            version = 5,
+            apiVersion = SOURCE_PLUGIN_API_VERSION,
+            runtime = PluginRuntime.DECLARATIVE,
+            hosts = setOf("knigavuhe.org", "m.knigavuhe.org"),
+            capabilities = setOf(SourceCapability.SERIES_SEARCH),
+            permissions = PluginPermissions(networkHosts = setOf("knigavuhe.org", "m.knigavuhe.org")),
+            entrypoints = mapOf("seriesSearch" to "search.rule")
+        )
+
+        val results = runtime.searchSeries(manifest, root, SeriesSearchQuery("Звездная кровь"))
+
+        assertEquals(2, results.size)
+        assertEquals("https://knigavuhe.org/series/zvezdnaja-krov/", results[0].series.url)
+        assertEquals("Звездная кровь", results[0].series.title)
+        assertEquals("https://knigavuhe.org/serie/legacy-cycle/", results[1].series.url)
+    }
+
     private inline fun withTempDir(block: (File) -> Unit) {
         val root = createTempDirectory("audoiboo-series-search-test-").toFile()
         try {

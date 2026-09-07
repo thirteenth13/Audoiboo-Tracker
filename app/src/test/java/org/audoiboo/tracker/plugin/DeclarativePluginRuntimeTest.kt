35975777fac3e69e494f39714ff441cb6ae6ed40
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-09-07 11:31:30 +0300

    Update Izib discovery tests for letter directory

--- app/src/test/java/org/audoiboo/tracker/plugin/DeclarativePluginRuntimeTest.kt
+++ app/src/test/java/org/audoiboo/tracker/plugin/DeclarativePluginRuntimeTest.kt
@@ -25,19 +25,11 @@ class DeclarativePluginRuntimeTest {
                 DeclarativeEntrypoint.SeriesLookup(
                     title = "h1",
                     description = ".desc",
-                    books = RepeatedFields(
-                        item = ".book",
-                        title = ".title",
-                        link = "a@href",
-                        author = ".author",
-                        number = ".num"
-                    )
+                    books = RepeatedFields(item = ".book", title = ".title", link = "a@href", author = ".author", number = ".num")
                 )
             }
         )
-
         val series = runtime.resolveSeries(manifest, root, "https://example.org/cycle")!!
-
         assertEquals("Star Blood", series.title)
         assertEquals("Cycle description", series.description)
         assertEquals(2, series.books.size)
@@ -54,37 +46,19 @@ class DeclarativePluginRuntimeTest {
             DeclarativeEntrypoint.SeriesLookup(
                 title = "h1",
                 books = RepeatedFields(item = ".book", title = ".title", link = "a@href"),
-                supplement = SeriesSupplement(
-                    startLink = "a.author@href",
-                    items = RepeatedFields(item = "article", title = "h2 a", link = "h2 a@href"),
-                    seriesTitle = ".series"
-                )
+                supplement = SeriesSupplement(startLink = "a.author@href", items = RepeatedFields(item = "article", title = "h2 a", link = "h2 a@href"), seriesTitle = ".series")
             )
         }
         val sandbox = PluginSandbox(PluginHttpTransport { request, _ ->
             val body = when (request.url) {
-                "https://example.org/series/star-blood" -> """
-                    <h1>Star Blood</h1>
-                    <a class='author' href='/author/a'>Author A</a>
-                    <div class='book'><a href='/book/10'><span class='title'>Roads</span></a></div>
-                """.trimIndent()
-                "https://example.org/author/a" -> """
-                    <article><h2><a href='/book/11'>Alpha Colony</a></h2><a class='series'>Star Blood</a></article>
-                    <article><h2><a href='/book/spin'>White Devil</a></h2><a class='series'>Star Blood. White Devil</a></article>
-                    <article><h2><a href='/book/other'>Other</a></h2><a class='series'>Other Series</a></article>
-                """.trimIndent()
+                "https://example.org/series/star-blood" -> "<h1>Star Blood</h1><a class='author' href='/author/a'>Author A</a><div class='book'><a href='/book/10'><span class='title'>Roads</span></a></div>"
+                "https://example.org/author/a" -> "<article><h2><a href='/book/11'>Alpha Colony</a></h2><a class='series'>Star Blood</a></article><article><h2><a href='/book/spin'>White Devil</a></h2><a class='series'>Star Blood. White Devil</a></article><article><h2><a href='/book/other'>Other</a></h2><a class='series'>Other Series</a></article>"
                 else -> error("unexpected ${request.url}")
             }
             PluginHttpResponse(200, request.url, body)
         })
-        val runtime = DeclarativePluginRuntime(sandbox, decoder)
-
-        val series = runtime.resolveSeries(manifest, root, "https://example.org/series/star-blood")!!
-
-        assertEquals(
-            listOf("https://example.org/book/10", "https://example.org/book/11"),
-            series.books.map { it.url }
-        )
+        val series = DeclarativePluginRuntime(sandbox, decoder).resolveSeries(manifest, root, "https://example.org/series/star-blood")!!
+        assertEquals(listOf("https://example.org/book/10", "https://example.org/book/11"), series.books.map { it.url })
     }
 
     @Test
@@ -92,12 +66,7 @@ class DeclarativePluginRuntimeTest {
         File(root, "series.rule").writeText("series")
         val manifest = manifest(entrypoints = mapOf("seriesLookup" to "series.rule"))
         val decoder = DeclarativeEntrypointDecoder {
-            DeclarativeEntrypoint.SeriesLookup(
-                title = "h1",
-                followLink = "a.series@href",
-                titleRegex = "(?i)series\\s+[\"«]?(.+?)[\"»]?\\s+listen",
-                books = RepeatedFields(item = "h2 a.book", title = "@text", link = "@href")
-            )
+            DeclarativeEntrypoint.SeriesLookup(title = "h1", followLink = "a.series@href", titleRegex = "(?i)series\\s+[\"«]?(.+?)[\"»]?\\s+listen", books = RepeatedFields(item = "h2 a.book", title = "@text", link = "@href"))
         }
         val sandbox = PluginSandbox(PluginHttpTransport { request, _ ->
             val body = when (request.url) {
@@ -107,52 +76,27 @@ class DeclarativePluginRuntimeTest {
             }
             PluginHttpResponse(200, request.url, body)
         })
-        val runtime = DeclarativePluginRuntime(sandbox, decoder)
-
-        val series = runtime.resolveSeries(manifest, root, "https://example.org/book/10")!!
-
+        val series = DeclarativePluginRuntime(sandbox, decoder).resolveSeries(manifest, root, "https://example.org/book/10")!!
         assertEquals("Star Blood", series.title)
         assertEquals("https://example.org/series/star-blood", series.url)
         assertEquals(listOf("https://example.org/book/10"), series.books.map { it.url })
     }
 
     @Test
-    fun discoversIzibSeriesThroughBoundedAuthorDirectory() {
-        val manifest = PluginPackageManifest(
-            id = "izib",
-            name = "Izib",
-            version = 3,
-            apiVersion = SOURCE_PLUGIN_API_VERSION,
-            hosts = setOf("pda.izib.uk", "izib.uk"),
-            capabilities = setOf(SourceCapability.SERIES_DISCOVERY),
-            permissions = PluginPermissions(networkHosts = setOf("pda.izib.uk", "izib.uk"))
-        )
+    fun discoversIzibSeriesThroughLetterAuthorDirectory() {
+        val manifest = PluginPackageManifest(id = "izib", name = "Izib", version = 3, apiVersion = SOURCE_PLUGIN_API_VERSION, hosts = setOf("pda.izib.uk", "izib.uk"), capabilities = setOf(SourceCapability.SERIES_DISCOVERY), permissions = PluginPermissions(networkHosts = setOf("pda.izib.uk", "izib.uk")))
         val requested = mutableListOf<String>()
         val sandbox = PluginSandbox(PluginHttpTransport { request, _ ->
             requested += request.url
             val body = when (request.url) {
-                "https://pda.izib.uk/authors" -> "<a href='/author1'>Other Author</a>"
-                "https://pda.izib.uk/authors?p=2" -> "<a href='/author2176'>Роман Прокофьев</a>"
-                "https://pda.izib.uk/author2176" -> """
-                    <a href='/serie8524'>Звездная Кровь</a>
-                    <a href='/serie9999'>Стеллар</a>
-                """.trimIndent()
+                "https://pda.izib.uk/authors?l=%D0%A0" -> "<a href='/author1'>Other Author</a>"
+                "https://pda.izib.uk/authors?l=%D0%9F" -> "<a href='/author2176'>Роман Прокофьев</a>"
+                "https://pda.izib.uk/author2176" -> "<a href='/serie8524'>Звездная Кровь</a><a href='/serie9999'>Стеллар</a>"
                 else -> error("unexpected ${request.url}")
             }
             PluginHttpResponse(200, request.url, body)
         })
-        val runtime = DeclarativePluginRuntime(sandbox)
-
-        val results = runtime.discoverIzibSeries(
-            manifest,
-            CanonicalSeriesMatchInput(
-                id = "star-blood",
-                title = "Звёздная кровь",
-                authors = listOf("Роман Прокофьев")
-            ),
-            maxAuthorPages = 2
-        )
-
+        val results = DeclarativePluginRuntime(sandbox).discoverIzibSeries(manifest, CanonicalSeriesMatchInput(id = "star-blood", title = "Звёздная кровь", authors = listOf("Роман Прокофьев")), maxAuthorPages = 2)
         assertEquals(1, results.size)
         assertEquals("https://pda.izib.uk/serie8524", results.single().series.url)
         assertEquals("Звездная Кровь", results.single().series.title)
@@ -161,156 +105,46 @@ class DeclarativePluginRuntimeTest {
 
     @Test
     fun izibDiscoveryStopsAtConfiguredAuthorPageBound() {
-        val manifest = PluginPackageManifest(
-            id = "izib",
-            name = "Izib",
-            version = 3,
-            apiVersion = SOURCE_PLUGIN_API_VERSION,
-            hosts = setOf("pda.izib.uk", "izib.uk"),
-            capabilities = setOf(SourceCapability.SERIES_DISCOVERY),
-            permissions = PluginPermissions(networkHosts = setOf("pda.izib.uk", "izib.uk"))
-        )
+        val manifest = PluginPackageManifest(id = "izib", name = "Izib", version = 3, apiVersion = SOURCE_PLUGIN_API_VERSION, hosts = setOf("pda.izib.uk", "izib.uk"), capabilities = setOf(SourceCapability.SERIES_DISCOVERY), permissions = PluginPermissions(networkHosts = setOf("pda.izib.uk", "izib.uk")))
         var requests = 0
-        val runtime = DeclarativePluginRuntime(PluginSandbox(PluginHttpTransport { request, _ ->
-            requests++
-            PluginHttpResponse(200, request.url, "<a href='/author1'>Someone Else</a>")
-        }))
-
-        val results = runtime.discoverIzibSeries(
-            manifest,
-            CanonicalSeriesMatchInput("c", "Star Blood", authors = listOf("Missing Author")),
-            maxAuthorPages = 3
-        )
-
+        val runtime = DeclarativePluginRuntime(PluginSandbox(PluginHttpTransport { request, _ -> requests++; PluginHttpResponse(200, request.url, "<a href='/author1'>Someone Else</a>") }))
+        val results = runtime.discoverIzibSeries(manifest, CanonicalSeriesMatchInput("c", "Star Blood", authors = listOf("Missing Author")), maxAuthorPages = 3)
         assertTrue(results.isEmpty())
-        assertEquals(3, requests)
+        assertEquals(2, requests)
     }
 
     @Test
     fun resolvesBookMetadataFromSelectorsInsideSandbox() = withTempDir { root ->
         File(root, "book.rule").writeText("book")
-        val manifest = manifest(
-            capabilities = setOf(SourceCapability.BOOK_LOOKUP),
-            entrypoints = mapOf("bookLookup" to "book.rule")
-        )
-        val runtime = runtime(
-            body = """
-                <html><body>
-                  <h1>Book One - Author A author Author A</h1>
-                  <a class='author'>Author A</a>
-                  <a class='series'>Star Blood</a>
-                  <span class='number'>3</span>
-                  <img class='cover' src='/covers/one.jpg'>
-                  <div class='desc'>Book description</div>
-                </body></html>
-            """.trimIndent(),
-            decoder = DeclarativeEntrypointDecoder {
-                DeclarativeEntrypoint.BookLookup(
-                    title = "h1",
-                    author = ".author",
-                    seriesTitle = ".series",
-                    seriesNumber = ".number",
-                    coverUrl = ".cover@src",
-                    description = ".desc",
-                    titleRegex = "^(.+?)(?=\\s+-\\s+.+?\\s+author\\b)"
-                )
-            }
-        )
-
+        val manifest = manifest(capabilities = setOf(SourceCapability.BOOK_LOOKUP), entrypoints = mapOf("bookLookup" to "book.rule"))
+        val runtime = runtime("<html><body><h1>Book One - Author A author Author A</h1><a class='author'>Author A</a><a class='series'>Star Blood</a><span class='number'>3</span><img class='cover' src='/covers/one.jpg'><div class='desc'>Book description</div></body></html>", DeclarativeEntrypointDecoder { DeclarativeEntrypoint.BookLookup(title = "h1", author = ".author", seriesTitle = ".series", seriesNumber = ".number", coverUrl = ".cover@src", description = ".desc", titleRegex = "^(.+?)(?=\\s+-\\s+.+?\\s+author\\b)") })
         val book = runtime.resolveBook(manifest, root, "https://example.org/book/1")!!
-
-        assertEquals("Book One", book.title)
-        assertEquals(listOf("Author A"), book.authors.map { it.name })
-        assertEquals("Star Blood", book.seriesTitle)
-        assertEquals(3.0, book.seriesNumber)
-        assertEquals("https://example.org/covers/one.jpg", book.coverUrl)
-        assertEquals("Book description", book.description)
+        assertEquals("Book One", book.title); assertEquals(listOf("Author A"), book.authors.map { it.name }); assertEquals("Star Blood", book.seriesTitle); assertEquals(3.0, book.seriesNumber); assertEquals("https://example.org/covers/one.jpg", book.coverUrl); assertEquals("Book description", book.description)
     }
 
     @Test
     fun resolvesArchiveCandidatesAndDeduplicatesUrls() = withTempDir { root ->
         File(root, "download.rule").writeText("downloads")
-        val manifest = manifest(
-            capabilities = setOf(SourceCapability.DOWNLOAD_RESOLUTION),
-            entrypoints = mapOf("downloadResolution" to "download.rule")
-        )
-        val runtime = runtime(
-            body = """
-                <a class='download' href='/files/book.zip'>one</a>
-                <a class='download' href='/files/book.zip'>duplicate</a>
-                <a class='download' href='/files/book2.zip'>two</a>
-            """.trimIndent(),
-            decoder = DeclarativeEntrypointDecoder {
-                DeclarativeEntrypoint.DownloadResolution(
-                    items = RepeatedFields(item = ".download", link = "@href"),
-                    type = DownloadType.ARCHIVE
-                )
-            }
-        )
-
+        val manifest = manifest(capabilities = setOf(SourceCapability.DOWNLOAD_RESOLUTION), entrypoints = mapOf("downloadResolution" to "download.rule"))
+        val runtime = runtime("<a class='download' href='/files/book.zip'>one</a><a class='download' href='/files/book.zip'>duplicate</a><a class='download' href='/files/book2.zip'>two</a>", DeclarativeEntrypointDecoder { DeclarativeEntrypoint.DownloadResolution(items = RepeatedFields(item = ".download", link = "@href"), type = DownloadType.ARCHIVE) })
         val results = runtime.resolveDownloads(manifest, root, "https://example.org/book")
-
-        assertEquals(2, results.size)
-        assertTrue(results.all { it.type == DownloadType.ARCHIVE })
-        assertEquals("https://example.org/files/book.zip", results[0].url)
+        assertEquals(2, results.size); assertTrue(results.all { it.type == DownloadType.ARCHIVE }); assertEquals("https://example.org/files/book.zip", results[0].url)
     }
 
     @Test
     fun refusesEntrypointOutsidePackageDirectory() = withTempDir { root ->
-        val manifest = manifest(entrypoints = mapOf("seriesLookup" to "../outside.rule"))
-        val runtime = runtime("<h1>x</h1>", DeclarativeEntrypointDecoder {
-            DeclarativeEntrypoint.SeriesLookup("h1")
-        })
-
-        assertThrows(PluginSandboxViolation::class.java) {
-            runtime.resolveSeries(manifest, root, "https://example.org/cycle")
-        }
+        val manifest = manifest(entrypoints = mapOf("seriesLookup" to "../outside.rule")); val runtime = runtime("<h1>x</h1>", DeclarativeEntrypointDecoder { DeclarativeEntrypoint.SeriesLookup("h1") })
+        assertThrows(PluginSandboxViolation::class.java) { runtime.resolveSeries(manifest, root, "https://example.org/cycle") }
     }
 
     @Test
     fun capabilityMustBeDeclared() = withTempDir { root ->
         File(root, "series.rule").writeText("series")
-        val manifest = manifest(
-            capabilities = setOf(SourceCapability.DOWNLOAD_RESOLUTION),
-            entrypoints = mapOf("seriesLookup" to "series.rule")
-        )
-        val runtime = runtime("<h1>x</h1>", DeclarativeEntrypointDecoder {
-            DeclarativeEntrypoint.SeriesLookup("h1")
-        })
-
-        assertThrows(PluginSandboxViolation::class.java) {
-            runtime.resolveSeries(manifest, root, "https://example.org/cycle")
-        }
+        val manifest = manifest(capabilities = setOf(SourceCapability.DOWNLOAD_RESOLUTION), entrypoints = mapOf("seriesLookup" to "series.rule")); val runtime = runtime("<h1>x</h1>", DeclarativeEntrypointDecoder { DeclarativeEntrypoint.SeriesLookup("h1") })
+        assertThrows(PluginSandboxViolation::class.java) { runtime.resolveSeries(manifest, root, "https://example.org/cycle") }
     }
 
-    private fun runtime(body: String, decoder: DeclarativeEntrypointDecoder): DeclarativePluginRuntime {
-        val sandbox = PluginSandbox(PluginHttpTransport { request, _ ->
-            PluginHttpResponse(200, request.url, body)
-        })
-        return DeclarativePluginRuntime(sandbox, decoder)
-    }
-
-    private fun manifest(
-        capabilities: Set<SourceCapability> = setOf(SourceCapability.SERIES_LOOKUP),
-        entrypoints: Map<String, String>
-    ) = PluginPackageManifest(
-        id = "declarative-test",
-        name = "Declarative test",
-        version = 1,
-        apiVersion = SOURCE_PLUGIN_API_VERSION,
-        runtime = PluginRuntime.DECLARATIVE,
-        hosts = setOf("example.org"),
-        capabilities = capabilities,
-        permissions = PluginPermissions(networkHosts = setOf("example.org")),
-        entrypoints = entrypoints
-    )
-
-    private inline fun withTempDir(block: (File) -> Unit) {
-        val root = createTempDirectory("audoiboo-declarative-runtime-test-").toFile()
-        try {
-            block(root)
-        } finally {
-            root.deleteRecursively()
-        }
-    }
+    private fun runtime(body: String, decoder: DeclarativeEntrypointDecoder): DeclarativePluginRuntime = DeclarativePluginRuntime(PluginSandbox(PluginHttpTransport { request, _ -> PluginHttpResponse(200, request.url, body) }), decoder)
+    private fun manifest(capabilities: Set<SourceCapability> = setOf(SourceCapability.SERIES_LOOKUP), entrypoints: Map<String, String>) = PluginPackageManifest(id = "declarative-test", name = "Declarative test", version = 1, apiVersion = SOURCE_PLUGIN_API_VERSION, runtime = PluginRuntime.DECLARATIVE, hosts = setOf("example.org"), capabilities = capabilities, permissions = PluginPermissions(networkHosts = setOf("example.org")), entrypoints = entrypoints)
+    private inline fun withTempDir(block: (File) -> Unit) { val root = createTempDirectory("audoiboo-declarative-runtime-test-").toFile(); try { block(root) } finally { root.deleteRecursively() } }
 }

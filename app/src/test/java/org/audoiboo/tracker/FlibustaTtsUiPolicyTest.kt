06eebf868bb930787e0c13309d223e87d883235c
thirteenth13 <31848255+thirteenth13@users.noreply.github.com>
2026-09-12 10:00:40 +0300

    Test strict Flibusta TTS URL policy

--- app/src/test/java/org/audoiboo/tracker/FlibustaTtsUiPolicyTest.kt
+++ app/src/test/java/org/audoiboo/tracker/FlibustaTtsUiPolicyTest.kt
@@ -8,15 +8,18 @@ class FlibustaTtsUiPolicyTest {
     @Test
     fun acceptsBookPagesFromSupportedFlibustaHosts() {
         assertTrue(isFlibustaBookUrl("https://flibusta.site/b/123"))
-        assertTrue(isFlibustaBookUrl("https://flibusta.one/b/abc-123/"))
+        assertTrue(isFlibustaBookUrl("http://flibusta.one/b/abc-123/"))
         assertTrue(isFlibustaBookUrl("https://flibusta.name/b/42"))
     }
 
     @Test
-    fun rejectsNonBookPagesAndLookalikeHosts() {
+    fun rejectsNonBookPagesLookalikeHostsSubdomainsAndNonWebSchemes() {
         assertFalse(isFlibustaBookUrl("https://flibusta.site/a/123"))
         assertFalse(isFlibustaBookUrl("https://flibusta.site/b/123/fb2"))
         assertFalse(isFlibustaBookUrl("https://evilflibusta.site/b/123"))
+        assertFalse(isFlibustaBookUrl("https://sub.flibusta.site/b/123"))
+        assertFalse(isFlibustaBookUrl("ftp://flibusta.site/b/123"))
+        assertFalse(isFlibustaBookUrl("file://flibusta.site/b/123"))
         assertFalse(isFlibustaBookUrl("https://example.org/b/123"))
         assertFalse(isFlibustaBookUrl("not a url"))
     }
